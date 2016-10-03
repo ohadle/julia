bb8cf48a8b864cac4a703d04982b9d7b2b1d7f21
Valentin Churavy <v.churavy@gmail.com>
2016-10-04 04:31:16 +0900

    deal with arch and os specific blacklists

--- deps/compiler-rt_standalone.mk
+++ deps/compiler-rt_standalone.mk
@@ -14,9 +14,10 @@
 SRCDIR := $(CRT_SRCDIR)/lib/builtins
 ARCH_SRCDIR := $(SRCDIR)/$(ARCH)
 INCLUDES := -I$(SRCDIR) -I$(ARCH_SRCDIR)
-CRT_CFLAGS := $(CPPFLAGS) $(CFLAGS) -O2 \
-		-std=gnu99 $(fPIC) -fno-builtin \
-		-ffreestanding $(INCLUDES)
+# TODO(vchuravy) discover architecture flags
+CRT_CFLAGS := $(CPPFLAGS) $(CFLAGS) -O2 -std=c11 \
+		$(fPIC) $(INCLUDES) \
+		-fno-builtin -ffreestanding
 ifeq ($(USE_CLANG),1)
 CRT_CFLAGS += -Wno-unknown-attributes -Wno-macro-redefined
 endif
@@ -25,18 +26,25 @@ endif
 # Blacklist a few files we don't want to deal with
 ##
 MAKEFLAGS := --no-builtin-rules
-BLACKLIST := atomic.c atomic_flag_clear.c atomic_flag_clear_explicit.c \
-	atomic_flag_test_and_set.c atomic_flag_test_and_set_explicit.c \
-	atomic_signal_fence.c atomic_thread_fence.c
+BLACKLIST := atomic.o atomic_flag_clear.o atomic_flag_clear_explicit.o \
+		atomic_flag_test_and_set.o atomic_flag_test_and_set_explicit.o \
+		atomic_signal_fence.o atomic_thread_fence.o
 
-# TODO(vchuravy) discover architecture flags
-# Discover all files...
+ifeq ($(ARCH),ppc)
+	BLACKLIST += saveFP.o restFP.o
+endif
+
+ifeq ($(OS),darwin)
+	# Which blacklist should we choose
+	BLACKLIST += $(shell cat $(SRCDIR)/Darwin-excludes/osx.txt)
+endif
+
 CFILES := $(wildcard $(SRCDIR)/*.c)
-GENERAL_OBJS1 := $(filter-out $(BLACKLIST:.c=.o), $(notdir $(CFILES:.c=.o)))
+GENERAL_OBJS1 := $(filter-out $(BLACKLIST), $(notdir $(CFILES:.c=.o)))
 
 ARCH_CFILES := $(wildcard $(ARCH_SRCDIR)/*.c)
 ARCH_SFILES := $(wildcard $(ARCH_SRCDIR)/*.S)
-ARCH_OBJS   := $(notdir $(join $(ARCH_CFILES:.c=.o),$(ARCH_SFILES:.S=.o)))
+ARCH_OBJS   := $(filter-out $(BLACKLIST), $(notdir $(join $(ARCH_CFILES:.c=.o),$(ARCH_SFILES:.S=.o))))
 
 GENERAL_OBJS := $(filter-out $(ARCH_OBJS), $(GENERAL_OBJS1))
 
